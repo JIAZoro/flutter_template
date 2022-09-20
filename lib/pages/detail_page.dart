03ee5d5f8a11ea136345098783882f5a3801fe2c
jjingwei <jia-j@aeonpeople.biz>
2022-09-20 17:25:25 +0800

    添加键盘工具

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template_plus/common/dismiss_keyboard.dart';
 import 'package:flutter_template_plus/generated/l10n.dart';
 import 'package:flutter_template_plus/pages/change_language_page.dart';
 import 'package:go_router/go_router.dart';
@@ -15,24 +16,28 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).detail),
-      ),
-      body: Column(
-        children: [
-          Center(
-            child: Text('传递过来的id数据: ${widget.id}'),
-          ),
-          TextButton(
-              onPressed: () {
-                // Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                //   return SettingLanguagePage();
-                // }));
-                context.go('/detail/setLanguage');
-              },
-              child: Text(S.current.set_language)),
-        ],
+    return KeyboardDismissOnTap(
+      dismissOnCapturedTaps: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(S.of(context).detail),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Text('传递过来的id数据: ${widget.id}'),
+            ),
+            TextButton(
+                onPressed: () {
+                  // Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  //   return SettingLanguagePage();
+                  // }));
+                  context.go('/detail/setLanguage');
+                },
+                child: Text(S.current.set_language)),
+                TextField()
+          ],
+        ),
       ),
     );
   }
